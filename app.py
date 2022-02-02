10c86ad2d8f0270655dd68ffa3f5f9ec30924b5a
santko <93163321+Santko@users.noreply.github.com>
2022-02-02 09:08:07 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Santko
+Aqui se muestra el precio **De cierre** Del Bitcoin en Dólares Americanos!
+""")
+
+#definir el ticker
+tickerSymbol = 'BTC-USD'
+#obtener datos
+tickerData = yf.Ticker(tickerSymbol)
+#obtener precios historicos
+tickerDf = tickerData.history(period='1d', start='2017-2-1', end='2022-2-1')
+
+
+st.write("""
+## Precio de cierre
+""")
+st.line_chart(tickerDf.Close)
